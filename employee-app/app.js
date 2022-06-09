21f5e3d2d252a21a0e8179270c2eb4d656c62000
Andi Hermanto <ahermanto47@gmail.com>
2022-06-09 09:42:11 -0400

    Add mongodb connection

--- employee-app/app.js
+++ employee-app/app.js
@@ -2,24 +2,47 @@ const express = require('express');
 const logger = require('morgan');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-
+// get MongoDB driver connection
+const dbo = require('./db/conn');
 const app = express();
 
 app.use(logger('dev'));
 
 //Swagger Configuration
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title:'Employee API',
-            version:'1.0.0'
-        }
-    },
-    apis:['app.js'],
-}
+//const swaggerOptions = {
+//    swaggerDefinition: {
+//        info: {
+//            title:'Employee API',
+//            version:'1.0.0'
+//        }
+//    },
+//    apis:['app.js'],
+//}
+//
+//const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
+//Openapi Configuration
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info : {
+			title: 'Employee API',
+			version: '1.0.0'
+		},
+	},
+	apis:['app.js'],
+};
+
+const swaggerDocs = swaggerJSDoc(options);
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+
+// perform a database connection when the server starts
+dbo.connectToServer(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+});
  
 /**
  * @swagger
