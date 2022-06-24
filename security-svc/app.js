00126edb74ea05e40f1cc7c7c7ab07a4e8eb8149
Andi Hermanto <ahermanto47@gmail.com>
2022-06-24 11:19:33 -0400

    First successful test

--- security-svc/app.js
+++ security-svc/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-module.exports = function() {
+module.exports = function(database) {
 
     const app = express();
     app.use(express.json());
     app.post('/users/signup', async (req, res) => {
-
+        const { username, password, email } = req.body;
+        database.createUser(username, password, email);
+        res.json({});
     })
 
     return app;
--- security-svc/app.test.js
+++ security-svc/app.test.js
@@ -1,13 +1,29 @@
-const request = require('supertest');
-const makeApp = require('./app.js');
+const request = require("supertest");
+const {jest} = require("@jest/globals")
+const {describe} = require("@jest/globals")
+const {test} = require("@jest/globals");
+const {expect} = require("expect");
+const makeApp = require("./app.js");
+
+const createUser = jest.fn();
+
+const app = makeApp(
+    {
+        createUser
+    }
+);
 
 describe("POST /users/signup", () => {
 
-    describe("given a username and password", () => {
-        test("should pass the username and password to the database createUser function", async () => {
-            const body = { username: "user1", password: "password1"};
+    describe("given a username, password, and email", () => {
+        test("should pass user data to database createUser function", async () => {
+            const body = { username: "user1", password: "password1", email: "test@dev.com"};
             await request(app).post("/users/signup").send(body);
             expect(createUser).toHaveBeenCalled();
+            expect(createUser.mock.calls.length).toBe(1);
+            expect(createUser.mock.calls[0][0]).toBe(body.username);
+            expect(createUser.mock.calls[0][1]).toBe(body.password);
+            expect(createUser.mock.calls[0][2]).toBe(body.email);
         })
     })
 
